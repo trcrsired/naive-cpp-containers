ae3e44233b7b1cb1a4a10e017dc8bb2a72fc836e
trcrsired <uwgghhbcad@gmail.com>
2022-06-13 17:23:03 -0400

    concepts

--- include/ncc_containers/impl/concepts.h
+++ include/ncc_containers/impl/concepts.h
@@ -4,12 +4,13 @@ namespace ncc
 {
 
 template<typename T>
-concept ncc_allocator=sizeof(T)<=(sizeof(void*)*2)&&::std::is_trivially_copyable_v<T>&&requires(T t,int* ptr,std::size_t n)
+concept ncc_allocator=sizeof(T)<=(sizeof(void*)*2)
+	&&::std::is_aggregate_v<T>&&::std::is_trivially_copyable_v<T>&&requires(T t,int* ptr,std::size_t n)
 {
-	{template t.allocate<int>(n)}->::std::same_as<int>;
-	{template t.deallocate<int>(ptr,n)}->::std::same_as<void>;
-	{template t.reallocate<int>(ptr,n)}->::std::same_as<int*>;
-	{template t.allocate_zero<int>(n)}->::std::same_as<int*>;
+	{t.template allocate<int>(n)}->::std::same_as<int>;
+	{t.template deallocate<int>(ptr,n)}->::std::same_as<void>;
+	{t.template reallocate<int>(ptr,n)}->::std::same_as<int*>;
+	{t.template allocate_zero<int>(n)}->::std::same_as<int*>;
 };
 
 template<typename T>
